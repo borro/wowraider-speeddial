f57b97b640d2b11817d49d70994186aa54d94fc8
Borro <borro@aggardo.ru>
2012-01-13 23:13:49 +0400

    Разделение css-файлов Дописал все потоки, кроме members Пофиксил баг, при выборе нужных рсс в настройках.

--- scripts/functions.js
+++ scripts/functions.js
@@ -20,22 +20,72 @@ function ajaxGet(url, successCallback)
 
 var feedsParsers = {
 	news: function (data) {
-		var body = initElements(data), ul = $('<ul></ul>');
-		for (var i = 0, maxi = data.data.length; i < maxi; i++) {
-			ul.append('<li><a href="'+data.data[i].url+'">'+data.data[i].title+'</a></li>')
-		}
-		body.append(ul);
-		return body;
+		return parseFeeds('news', data);
 	},
 	blogs: function (data) {
-		var body = initElements(data), ul = $('<ul></ul>');
+		return parseFeeds('blogs', data);
+	},
+	questions: function (data) {
+		return parseFeeds('questions', data);
+	},
+	streams: function (data) {
+		return parseFeeds('streams', data);
+	},
+	'blues:ru': function (data) {
+		return parseFeeds('blues:ru', data);
+	}, 
+	'blues:ru:bluestart': function (data) {
+		return parseFeeds('blues:ru:bluestart', data);
+	}, 
+	'blues:en': function (data) {
+		return parseFeeds('blues:en', data);
+	}, 
+	'blues:en:bluestart': function (data) {
+		return parseFeeds('blues:en:bluestart', data);
+	},
+	plf: function (data) {
+		var body = initElements('plf', data), table = $('<table></table>');
+		for (var i = 0, maxi = data.data.length; i < maxi; i++) {
+			table.append('<tr><td>'+data.data[i].title+'</td><td>'+data.data[i].number+'</td></tr>');
+		}
+		body.append(table);
 		return body;
+	},
+	glf: function (data) {
+		return parseLF('glf', data);
+	},
+	slf: function (data) {
+		return parseLF('slf', data);
 	}
+};
+
+function initElements(type, data)
+{
+	var body = $('<div class="'+type.split(':').join(' ')+'"></div>');
+	body.append('<h1>'+data.title+'</h1>');
+	return body;
 }
 
-function initElements(data)
-{
-	var body = $('<div class="news"></div>');
-	body.append('<h1>'+data.title+'</h1>');
+function parseFeeds(type, data) {
+	var body = initElements(type, data), ul = $('<ul></ul>');
+	if (data.data.length) {
+		for (var i = 0, maxi = data.data.length; i < maxi; i++) {
+			ul.append('<li><a href="'+data.data[i].url+'">'+data.data[i].title+'</a></li>');
+		}
+	} else {
+		ul.append('<li>нет данных</li>');
+	}
+	body.append(ul);
+	return body;
+}
+
+function parseLF(type, data) {
+	var body = initElements(type, data);
+	for (var i = 0, maxi = data.data.length; i < maxi; i++) {
+		body.append(
+			'<div><img src="/images/wowicons/class/'+data.data[i].classid+'.gif" alt="'+data.data[i].title+'">'
+			+ '<span style="color:#'+data.data[i].classcolor+';">'+data.data[i].number+'</span></div>'
+		);
+	}
 	return body;
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,34 @@
 window.addEventListener('load', function() {
 	//стандартные настройки
-	var feeds = widget.preferences.feed || 'news',
+	var feeds = (widget.preferences.feed || 'news').split(','),
 		feedIntreval = widget.preferences.interval || 3600,
-		raw_data = {},
 		content = {},
 		feeds_i = 0,
-		feedsArray = feeds.split(',');
+		intervalIdStorage = null,
+		intervalIdRotate = null;
 
 	function changeContent() {
 		var i = feeds_i++;
-		if (content[feedsArray[i]] == undefined) return;
-		$('output').html(content[feedsArray[i]]);
-		setSpeedDial(raw_data[feedsArray[i]].title, raw_data[feedsArray[i]].url);
-		if (feeds_i >= feedsArray.length) {
+		if (content[feeds[i]] == undefined) return;
+		$('output').html(content[feeds[i]].data);
+		setSpeedDial(content[feeds[i]].title, content[feeds[i]].url);
+		if (feeds_i >= feeds.length) {
 			feeds_i = 0;
 		}
 	}
 
 	function getContent()
 	{
-		ajaxGet('http://wowraider.ru/common/api.cdiml?data='+feeds, function (data) {
-			raw_data = data = $.parseJSON(data);
+		ajaxGet('http://wowraider.ru/common/api.cdiml?data='+feeds.join(','), function (data) {
+			data = $.parseJSON(data);
+			content = {};
 			for (var name in data) if (data.hasOwnProperty(name)) {
 				if (feedsParsers[name]) {
-					content[name] = feedsParsers[name](data[name]);
+					content[name] = {
+						title: data[name].title,
+						url: data[name].url,
+						data: feedsParsers[name](data[name])
+					};
 				}
 			}
 			feeds_i = 0;
@@ -33,13 +38,17 @@ window.addEventListener('load', function() {
 
 	//следим за изменение настроек
 	addEventListener('storage', function () {
-		feeds = widget.preferences.feed || 'news';
+		feeds = (widget.preferences.feed || 'news').split(',');
 		feedIntreval = widget.preferences.interval || 3600;
-		getContent();
+		if (intervalIdStorage) clearTimeout(intervalIdStorage);
+		intervalIdStorage = setTimeout(function() {
+			getContent();
+			if (intervalIdRotate) clearInterval(intervalIdRotate);
+			intervalIdRotate = setInterval(changeContent, 3000);
+		}, 500);
 	}, false);
 
 	//меняем контент каждые 10 секунд
-	setInterval(changeContent, 10000);
-
+	intervalIdRotate = setInterval(changeContent, 3000);
 	getContent();
 });
