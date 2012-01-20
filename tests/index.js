ee32ee7ba48cdc21770f71e0eeabec7342ab6ff0
Borro <boris@artektiv.ru>
2012-01-20 14:54:02 +0400

    Сделан тест "Корректность получения всех лент" Наброски теста "Смена лент в зависимости от интервала"

--- tests/index.js
+++ tests/index.js
@@ -1,13 +1,14 @@
 window.debug_extention = function() {
 	//рабочий див
-	var debug_div = $('<div></div>').css({
-		position: 'absolute',
-		left: 0,
-		top: 0,
-		width: '100%',
-		height: '100%',
-		zIndex: '2'
-	}).appendTo('body').append('<h1 style="color: #f00;">Режим тестирования</h1>');
+	var tests_div = $('<div></div>').css({
+			position: 'absolute',
+			left: 0,
+			top: 0,
+			width: '100%',
+			height: '100%',
+			zIndex: '2'
+		}).appendTo('body').append('<h1 style="color: #f00;">Режим тестирования</h1>'),
+		tests_ol = $('<ol></ol>').appendTo(tests_div);
 	
 	//затемнение
 	$('<div></div>').css({
@@ -16,8 +17,8 @@ window.debug_extention = function() {
 		top: 0,
 		width: '100%',
 		height: '100%',
-		background: '#fff',
-		opacity: '0.5',
+		background: '#000',
+		opacity: '0.8',
 		zIndex: '1'
 	}).appendTo('body');
 	
@@ -25,9 +26,13 @@ window.debug_extention = function() {
 	$('head').append('<script src="/tests/tests.js"></script>');
 	
 	//прогон тестов
-	var result = null;
+	console.log('Старт тестов:');
 	for (var test in tests_list) {
-		result = tests_list[test]();
-		console.log(result);
+		tests_list[test](function(result) {
+			var text = result.name + ': ' + result.result + (result.result != 'pass' ? ' ('+result.description.join("\n")+')' : '');
+			tests_ol.append('<li>'+text+'</li>');
+			if (result.result == 'pass') console.log(text);
+			else console.error(text);
+		});
 	}
 };
--- tests/tests.js
+++ tests/tests.js
@@ -1,25 +1,146 @@
 var tests_list = {
-	correctness_feeds: function() {
-		var test_result = 'fail', descr = 'default';
+	correctness_feeds: function(callback) {
+		var test_result = {name: 'Корректность получения всех лент', result:'pass', description: []};
 		$.ajax({
 			url: 'http://wowraider.ru/api',
 			dataType: 'json',
-			async: false,
 			data: {data: 'news,blues:ru,blues:ru:bluestart,blues:en,blues:en:bluestart,blogs,posts,comments,streams,questions,member,plf,glf,slf'},
 			success: function (data, textStatus, jqXHR) {
 				var feeds_count = 0;
-				for ( var i in data) ++feeds_count;
-				if (feeds_count != 14) {
-					test_result = 'fail';
-					descr = 'В объекте всего '+feeds_count+' элементов';
+				for (var i in data) {
+					if (data[i].title === undefined) {
+						test_result.result = 'fail';
+						test_result.description.push('Отсутствует title в ленте '+i);
+					}
+					if (data[i].url === undefined) {
+						test_result.result = 'fail';
+						test_result.description.push('Отсутствует url в ленте '+i);
+					}
+					if (data[i].data === undefined) {
+						test_result.result = 'fail';
+						test_result.description.push('Отсутствует data в ленте '+i);
+					} else {
+						if (!(data[i].data instanceof Array)) {
+							test_result.result = 'fail';
+							test_result.description.push('Data в ленте '+i+' не является массивом');
+						} else if (data[i].data.length > 0) {
+							switch(i) {
+								case 'news':
+								case 'blues:ru':
+								case 'blues:ru:bluestart':
+								case 'blues:en':
+								case 'blues:en:bluestart':
+								case 'blogs':
+								case 'posts':
+								case 'comments':
+								case 'streams':
+								case 'questions':
+									if (data[i].data[0].title === undefined) {
+										test_result.result = 'fail';
+										test_result.description.push('Отсутствует title в data ленты '+i);
+									}
+									if (data[i].data[0].url === undefined) {
+										test_result.result = 'fail';
+										test_result.description.push('Отсутствует url в data ленты '+i);
+									}
+
+									++feeds_count;
+								break;
+								case 'member':
+									if (data[i].data[0].online === undefined) {
+										test_result.result = 'fail';
+										test_result.description.push('Отсутствует online в data ленты '+i);
+									}
+									if (data[i].data[0].newmessages === undefined) {
+										test_result.result = 'fail';
+										test_result.description.push('Отсутствует newmessages в data ленты '+i);
+									}
+									++feeds_count;
+								break;
+								case 'plf':
+									if (data[i].data[0].title === undefined) {
+										test_result.result = 'fail';
+										test_result.description.push('Отсутствует title в data ленты '+i);
+									}
+									if (data[i].data[0].number === undefined) {
+										test_result.result = 'fail';
+										test_result.description.push('Отсутствует number в data ленты '+i);
+									}
+									++feeds_count;
+								break;
+								case 'glf':
+								case 'slf':
+									if (data[i].data[0].title === undefined) {
+										test_result.result = 'fail';
+										test_result.description.push('Отсутствует title в data ленты '+i);
+									}
+									if (data[i].data[0].number === undefined) {
+										test_result.result = 'fail';
+										test_result.description.push('Отсутствует number в data ленты '+i);
+									}
+									if (data[i].data[0].classid === undefined) {
+										test_result.result = 'fail';
+										test_result.description.push('Отсутствует classid в data ленты '+i);
+									}
+									if (data[i].data[0].classcolor === undefined) {
+										test_result.result = 'fail';
+										test_result.description.push('Отсутствует classcolor в data ленты '+i);
+									}
+									++feeds_count;
+								break;
+							}
+						} else {
+							++feeds_count;
+						}
+					}
+
 				}
+				if (feeds_count != 14) {
+					test_result.result = 'fail';
+					test_result.description.push('В объекте всего '+feeds_count+' элементов');
+				}
+				callback(test_result);
 			},
 			error: function(jqXHR, textStatus, errorThrown) {
 				test_result = 'fail';
-				descr = 'jQuery.ajax: ' + textStatus;
+				test_result.description.push('jQuery.ajax: ' + textStatus);
+				callback(test_result);
 			}
 		});
-		
-		return {name: 'Корректность получения всех лент', result: test_result, description: descr};
+	},
+	change_feeds_interval: function(callback) {
+		var test_result = {name: 'Смена лент в зависимости от интервала', result: 'pass', description: []},
+			intervalValueFail = 0,
+			count = 0,
+			tmp_content = $('output').html(),
+			start_time = (new Date()).getTime();
+
+		var checkIntervalId = setInterval(function(){
+			console.log((new Date()).getTime() - start_time);
+			if (tmp_content != $('output').html() && tmp_content != '') {
+				var time = (new Date()).getTime() - start_time;
+				if (!(- 100 <= time && time <=  widget.preferences.frequency_change*1000 + 100)) {
+					++intervalValueFail;
+				} else {
+					tmp_content = $('output').html();
+					start_time = (new Date()).getTime();
+				}
+			} else if (tmp_content == '') {
+				start_time = (new Date()).getTime();
+				tmp_content = $('output').html();
+			} else {
+				tmp_content = $('output').html();
+			}
+
+			if (intervalValueFail > 1) {
+				test_result.result = 'fail';
+				test_result.description.push('Превышено количество непройденных интервалов');
+				clearInterval(checkIntervalId);
+				callback(test_result);
+			}
+
+		}, 100);
+
+
 	}
 };
